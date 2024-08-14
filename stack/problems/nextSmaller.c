d0817359d396e22bdf48b03fd25dbb39f93f7105
Divyayush Verma <122596172+divyayush-v-20@users.noreply.github.com>
2024-08-14 09:57:10 +0530

    Create nextSmaller.c

---
+++ stack/problems/nextSmaller.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../stack.h"
+
+int* nextSmaller(int* arr, int n){
+    int* ans = (int*)malloc(n* sizeof(int));
+    for(int i = 0; i < n; i++) ans[i] = -1;
+
+    for(int i = n - 1; i >= 0; i--){
+        while(!isEmpty() && arr[i] <= top()) pop();
+        if(!isEmpty()) ans[i] = top();
+        push(arr[i]);
+    }
+    return ans;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+
+    int* arr = (int*)malloc(n* sizeof(int));
+    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    int* ans = nextSmaller(arr, n);
+    for(int i = 0; i < n; i++) printf("%d ", ans[i]);
+
+    free(arr);
+    free(ans);
+
+    return 0;
+}
