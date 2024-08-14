ff7b782268fce4c445609fd91e169a945d3de71f
Divyayush Verma <122596172+divyayush-v-20@users.noreply.github.com>
2024-08-14 09:44:53 +0530

    Update nextGreater.c

--- stack/problems/nextGreater.c
+++ stack/problems/nextGreater.c
@@ -7,7 +7,7 @@ int* nextGreater(int* arr, int n){
     for(int i = 0; i < n; i++) ans[i] = -1;
 
     for(int i = n - 1; i >= 0; i--){
-        while(!isEmpty() && arr[i] > top()){
+        while(!isEmpty() && arr[i] >= top()){
             pop();
         }
         if(!isEmpty()) ans[i] = top();
