5d56fda406743def5a57635fc5bbab7251368af8
Divyayush Verma <122596172+divyayush-v-20@users.noreply.github.com>
2024-08-17 12:01:06 +0530

    17 aug lect1

--- web_d/js_basics/map.js
+++ web_d/js_basics/map.js
@@ -1,6 +1,6 @@
 let x = new Map();
-x.set({a : 1});
+x.set({"a" : 1, "b" : 2, "c" : 3});
 
-for(let val in x.values()){
-    console.log(val);   
+for(let [key, val] of x){
+    console.log(key, val);   
 }
